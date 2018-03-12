3b29bc31ba8ce0bb76c53d59936fc32ab48e80dd
Kevin Huang <yellow.khaos@gmail.com>
2018-03-12 01:33:49 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,15 @@
 class Team
+  attr_accessor :name, :motto
+  TEAM
+
+    def initialize (params)
+      params.each {|key, value| self.send("#{key}=",value)}
+      TEAM<<self
+    end
+
+    def self.all
+      TEAM
+    end
+  end
 
 end
