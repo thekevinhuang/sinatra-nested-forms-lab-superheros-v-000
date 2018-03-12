f39c6925c701ff645bed8f1bcfc4f3caf9197640
Kevin Huang <yellow.khaos@gmail.com>
2018-03-12 01:32:41 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,8 +1,13 @@
 class Superhero
   attr_accessor :name, :power, :biography
+  SUPERHEROES
 
   def initialize (params)
     params.each {|key, value| self.send("#{key}=",value)}
+    SUPERHEROES<<self
   end
 
+  def self.all
+    SUPERHEROES
+  end
 end
